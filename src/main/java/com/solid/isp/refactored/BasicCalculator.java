b041cdaf8c5a6ae4af785eda030cf55ef65fd715
Martin Villa Sono <mvillasono@gmail.com>
2025-06-29 13:31:34 -0500

    Interface segregation principle

---
+++ src/main/java/com/solid/isp/refactored/BasicCalculator.java
@@ -0,0 +1,23 @@
+package com.solid.isp.refactored;
+
+public class BasicCalculator implements BasicOperations{
+    @Override
+    public Double add(Double a, Double b) {
+        return a + b;
+    }
+
+    @Override
+    public Double subtract(Double a, Double b) {
+        return a - b;
+    }
+
+    @Override
+    public Double multiply(Double a, Double b) {
+        return a * b;
+    }
+
+    @Override
+    public Double divide(Double a, Double b) {
+        return a / b;
+    }
+}
---
+++ src/main/java/com/solid/isp/refactored/TrigonometricOperations.java
@@ -0,0 +1,7 @@
+package com.solid.isp.refactored;
+
+public interface TrigonometricOperations {
+    public Double sine(Double angle);
+
+    public Double cosine(Double angle);
+}
